cad1f42e8e9d82b01c5608866754aee0c1b3aef3
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2021-05-05 12:08:04 +0200

    Added possibility to set the path to the data directory in CMake.

--- src/Main.cpp
+++ src/Main.cpp
@@ -23,6 +23,11 @@ int main(int argc, char *argv[]) {
     AppSettings::get()->loadSettings(settingsFile.c_str());
     AppSettings::get()->getSettings().addKeyValue("window-multisamples", 0);
     //AppSettings::get()->getSettings().addKeyValue("windowSettings.debugContext", true);
+#ifdef DATA_PATH
+    if (!sgl::FileUtils::get()->directoryExists("Data") && !sgl::FileUtils::get()->directoryExists("../Data")) {
+        sgl::AppSettings::get()->setDataDirectory(DATA_PATH);
+    }
+#endif
     AppSettings::get()->setLoadGUI();
 
     Window *window = AppSettings::get()->createWindow();
