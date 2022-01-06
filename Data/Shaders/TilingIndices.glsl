87de73664e008f8cd5a3a88fd17b9d6a3d648b39
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2022-01-06 21:53:56 +0100

    Adapted OpenGL shader uniform and input attribute names to improve compatibility with recent Intel Linux drivers.

--- Data/Shaders/TilingIndices.glsl
+++ Data/Shaders/TilingIndices.glsl
@@ -1,11 +1,38 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2018 - 2021, Christoph Neuhauser
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 -- Vertex
 
 #version 430 core
 
 layout(location = 0) in vec3 vertexPosition;
 
-void main()
-{
+void main() {
     gl_Position = mvpMatrix * vec4(vertexPosition, 1.0);
 }
 
@@ -33,8 +60,7 @@ uniform vec4 color;
  * gl_WarpsPerSMNV: Maximum number of warp executing on a SM (streaming multiprocessor).
  * gl_WarpSizeNV: Number of SMs on the GPU.
  */
-void main()
-{
+void main() {
     float threadNum = float(gl_ThreadInWarpNV)/float(gl_WarpSizeNV-1u);
     float warpNum = float(gl_WarpIDNV)/float(gl_WarpsPerSMNV-1u);
     float smNum = float(gl_SMIDNV)/float(gl_SMCountNV-1u);
@@ -49,4 +75,3 @@ void main()
         fragColor = vec4(smNum * color.rgb, color.a);
     }
 }
-
--- Data/Shaders/TilingTiles.glsl
+++ Data/Shaders/TilingTiles.glsl
@@ -1,3 +1,31 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2018 - 2021, Christoph Neuhauser
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 -- Vertex
 
 #version 430 core
@@ -7,8 +35,7 @@ layout(location = 1) in vec3 vertexColor;
 
 out vec4 triangleColor;
 
-void main()
-{
+void main() {
     triangleColor = vec4(vertexColor, 1.0);
     gl_Position = mvpMatrix * vec4(vertexPosition, 1.0);
 }
@@ -26,8 +53,7 @@ layout(binding = 0, offset = 0) uniform atomic_uint fragCounter;
 // Maximum number of pixels to render in one frame
 uniform uint maxNumPixels;
 
-void main()
-{
+void main() {
     uint insertIndex = atomicCounterIncrement(fragCounter);
     if (insertIndex >= maxNumPixels) {
         discard;
